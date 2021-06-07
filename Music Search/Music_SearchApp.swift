4b5022f6439132640f922b871fedcb358e9951d3
Jade Jamig <ajamig016@gmail.com>
2021-06-07 12:05:29 +0800

    Initial Commit

---
+++ Music Search/Music_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_SearchApp.swift
+//  Music Search
+//
+//  Created by Anselm Jade Jamig on 6/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Music_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
